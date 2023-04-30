fedd74c9e9c4e9085ab117eafc221cb3cb626cec
venkat <venkatkilari5@gmail.com>
2023-04-30 10:43:06 +0530

    main

---
+++ src/main/java/com/ait/binding/JsonToJavaConvertion.java
@@ -0,0 +1,23 @@
+package com.ait.binding;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonToJavaConvertion {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		
+		File f=new File("customer.json");
+		
+		ObjectMapper mapper =new ObjectMapper();
+		
+		Customer c = mapper.readValue(f, Customer.class);
+		
+		System.out.println(c);
+
+	}
+
+}
