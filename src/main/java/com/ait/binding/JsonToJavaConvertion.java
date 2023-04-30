c81f6d54d5ce34eb269827512631b2fede628afb
venkat4-sys <118825230+venkat4-sys@users.noreply.github.com>
2023-04-30 10:46:05 +0530

    Update JsonToJavaConvertion.java

--- src/main/java/com/ait/binding/JsonToJavaConvertion.java
+++ src/main/java/com/ait/binding/JsonToJavaConvertion.java
@@ -9,11 +9,11 @@ public class JsonToJavaConvertion {
 	public static void main(String[] args) throws Exception {
 		
 		
-		
+		//source file
 		File f=new File("customer.json");
 		
 		ObjectMapper mapper =new ObjectMapper();
-		
+		//convertion of json to java object
 		Customer c = mapper.readValue(f, Customer.class);
 		
 		System.out.println(c);
